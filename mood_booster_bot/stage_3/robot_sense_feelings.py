18119dd721bb780bde69aa351f76e0a5fa922d03
Tim Kurian <54671289+timkurian@users.noreply.github.com>
2024-02-28 12:40:52 +0000

    Create robot_sense_feelings.py

---
+++ mood_booster_bot/stage_3/robot_sense_feelings.py
@@ -0,0 +1,22 @@
+feelings= {
+        'good': ['good','fantastic', 'very well'],
+        'bad': ['bad', 'not good', 'sad' , 'mad'],
+        'ok': ['ok', 'fine', 'not sure']
+    }
+
+
+name = input('What is your name?')
+print(f'Hello {name}, I am Robot.')
+feeling = input('How are you feeling?')
+
+if feeling in feelings['good']:
+      print(f'Glad to see you are feeling {feeling}')
+elif feeling in feelings['ok']:
+      print('Reach for the stars!' )
+elif feeling in feelings['bad']:
+      print(f'Oh, sorry to hear you are feeling {feeling} ')
+else:
+      category = input(f'I do not know what {feeling} is. Is it good / bad or ok ?')
+      handle_new_feeling(feeling, category)
+           
+   
