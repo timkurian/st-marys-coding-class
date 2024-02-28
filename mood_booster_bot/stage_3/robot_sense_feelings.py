119f1cc34d0fb09fa6598e09aaedb9c025b5a285
Tim Kurian <54671289+timkurian@users.noreply.github.com>
2024-02-28 15:32:10 +0000

    Fix to stage 3

--- mood_booster_bot/stage_3/robot_sense_feelings.py
+++ mood_booster_bot/stage_3/robot_sense_feelings.py
@@ -4,19 +4,22 @@ feelings= {
         'ok': ['ok', 'fine', 'not sure']
     }
 
-
-name = input('What is your name?')
-print(f'Hello {name}, I am Robot.')
-feeling = input('How are you feeling?')
-
-if feeling in feelings['good']:
-      print(f'Glad to see you are feeling {feeling}')
-elif feeling in feelings['ok']:
-      print('Reach for the stars!' )
-elif feeling in feelings['bad']:
-      print(f'Oh, sorry to hear you are feeling {feeling} ')
-else:
-      category = input(f'I do not know what {feeling} is. Is it good / bad or ok ?')
-      handle_new_feeling(feeling, category)
+while True:
+    name = input('What is your name?')
+    print(f'Hello {name}, I am Robot.')
+    feeling = input('How are you feeling?')
+    
+    if feeling in feelings['good']:
+          print(f'Glad to see you are feeling {feeling}')
+    elif feeling in feelings['ok']:
+          print('Reach for the stars!' )
+    elif feeling in feelings['bad']:
+          print(f'Oh, sorry to hear you are feeling {feeling} ')
+    else:
+          category = input(f'I do not know what {feeling} is. Is it good / bad or ok ?')
+          if category in feelings.keys():
+               feelings[category].append(feeling)
+          else:
+                print(f'I did not understand')
            
    
